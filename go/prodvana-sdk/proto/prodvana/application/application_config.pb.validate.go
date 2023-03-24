4859f2aae44a5596efe1a7144fba6581bc09af07
Naphat Sanguansin <naphat@prodvana.io>
2023-03-23 18:13:16 -0700

    delete pipeline templates from app config
     betamike
    
    GitOrigin-RevId: 3033959dc536429c4fb73b751649b12d5fadc63f

--- go/prodvana-sdk/proto/prodvana/application/application_config.pb.validate.go
+++ go/prodvana-sdk/proto/prodvana/application/application_config.pb.validate.go
@@ -113,40 +113,6 @@ func (m *ApplicationConfig) validate(all bool) error {
 
 	}
 
-	for idx, item := range m.GetPipelineTemplates() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, ApplicationConfigValidationError{
-						field:  fmt.Sprintf("PipelineTemplates[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, ApplicationConfigValidationError{
-						field:  fmt.Sprintf("PipelineTemplates[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return ApplicationConfigValidationError{
-					field:  fmt.Sprintf("PipelineTemplates[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	if all {
 		switch v := interface{}(m.GetNotifications()).(type) {
 		case interface{ ValidateAll() error }:
