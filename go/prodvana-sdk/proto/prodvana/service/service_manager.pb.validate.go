9fa2bc79dea7376f8364029f0e07ed3e80517caa
Naphat Sanguansin <naphat@prodvana.io>
2023-03-29 10:40:42 -0700

    return version metadata in list rpc
    GitOrigin-RevId: cde0b9bf1c002095df2551b411bbebbd752f2d82

--- go/prodvana-sdk/proto/prodvana/service/service_manager.pb.validate.go
+++ go/prodvana-sdk/proto/prodvana/service/service_manager.pb.validate.go
@@ -5513,6 +5513,37 @@ func (m *ListServiceConfigVersionsResp_VersionMetadata) validate(all bool) error
 		}
 	}
 
+	// no validation rules for Source
+
+	if all {
+		switch v := interface{}(m.GetSourceMetadata()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ListServiceConfigVersionsResp_VersionMetadataValidationError{
+					field:  "SourceMetadata",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ListServiceConfigVersionsResp_VersionMetadataValidationError{
+					field:  "SourceMetadata",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetSourceMetadata()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ListServiceConfigVersionsResp_VersionMetadataValidationError{
+				field:  "SourceMetadata",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return ListServiceConfigVersionsResp_VersionMetadataMultiError(errors)
 	}
