d92263e8f2901ebdce664621559a9ab9b1eef873
Anup Chenthamarakshan <anup@prodvana.io>
2023-04-05 16:25:49 -0700

    Action Explanations - not quite full feature
    - Basic action explanation
    - Fixup timestamp
    - Plumb action explanation through
    - Checkpoint - figure out proxy stuff
    - Pass up debug logs
    
     naphatkrit
    
    GitOrigin-RevId: f51c5a6fccd5b6ed2a9744c8ddb9f50619ec669a

--- go/prodvana-sdk/proto/prodvana/desired_state/manager.pb.validate.go
+++ go/prodvana-sdk/proto/prodvana/desired_state/manager.pb.validate.go
@@ -725,6 +725,139 @@ var _ interface {
 	ErrorName() string
 } = StatusExplanationsValidationError{}
 
+// Validate checks the field values on DebugLogs with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *DebugLogs) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on DebugLogs with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in DebugLogsMultiError, or nil
+// if none found.
+func (m *DebugLogs) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *DebugLogs) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	for idx, item := range m.GetDebugLogs() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, DebugLogsValidationError{
+						field:  fmt.Sprintf("DebugLogs[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, DebugLogsValidationError{
+						field:  fmt.Sprintf("DebugLogs[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return DebugLogsValidationError{
+					field:  fmt.Sprintf("DebugLogs[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return DebugLogsMultiError(errors)
+	}
+
+	return nil
+}
+
+// DebugLogsMultiError is an error wrapping multiple validation errors returned
+// by DebugLogs.ValidateAll() if the designated constraints aren't met.
+type DebugLogsMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m DebugLogsMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m DebugLogsMultiError) AllErrors() []error { return m }
+
+// DebugLogsValidationError is the validation error returned by
+// DebugLogs.Validate if the designated constraints aren't met.
+type DebugLogsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e DebugLogsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e DebugLogsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e DebugLogsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e DebugLogsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e DebugLogsValidationError) ErrorName() string { return "DebugLogsValidationError" }
+
+// Error satisfies the builtin error interface
+func (e DebugLogsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sDebugLogs.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = DebugLogsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = DebugLogsValidationError{}
+
 // Validate checks the field values on DesiredStateSummary with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -1006,6 +1139,98 @@ func (m *DesiredStateSummary) validate(all bool) error {
 				if err := v.Validate(); err != nil {
 					return DesiredStateSummaryValidationError{
 						field:  fmt.Sprintf("StatusExplanations[%v]", key),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
+	}
+
+	{
+		sorted_keys := make([]string, len(m.GetDebugLogs()))
+		i := 0
+		for key := range m.GetDebugLogs() {
+			sorted_keys[i] = key
+			i++
+		}
+		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
+		for _, key := range sorted_keys {
+			val := m.GetDebugLogs()[key]
+			_ = val
+
+			// no validation rules for DebugLogs[key]
+
+			if all {
+				switch v := interface{}(val).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, DesiredStateSummaryValidationError{
+							field:  fmt.Sprintf("DebugLogs[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, DesiredStateSummaryValidationError{
+							field:  fmt.Sprintf("DebugLogs[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return DesiredStateSummaryValidationError{
+						field:  fmt.Sprintf("DebugLogs[%v]", key),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
+	}
+
+	{
+		sorted_keys := make([]string, len(m.GetActionExplanations()))
+		i := 0
+		for key := range m.GetActionExplanations() {
+			sorted_keys[i] = key
+			i++
+		}
+		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
+		for _, key := range sorted_keys {
+			val := m.GetActionExplanations()[key]
+			_ = val
+
+			// no validation rules for ActionExplanations[key]
+
+			if all {
+				switch v := interface{}(val).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, DesiredStateSummaryValidationError{
+							field:  fmt.Sprintf("ActionExplanations[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, DesiredStateSummaryValidationError{
+							field:  fmt.Sprintf("ActionExplanations[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return DesiredStateSummaryValidationError{
+						field:  fmt.Sprintf("ActionExplanations[%v]", key),
 						reason: "embedded message failed validation",
 						cause:  err,
 					}
--- go/prodvana-sdk/proto/prodvana/desired_state/model/desired_state.pb.validate.go
+++ go/prodvana-sdk/proto/prodvana/desired_state/model/desired_state.pb.validate.go
@@ -702,6 +702,141 @@ var _ interface {
 	ErrorName() string
 } = StatusExplanationValidationError{}
 
+// Validate checks the field values on ActionExplanation with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *ActionExplanation) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ActionExplanation with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ActionExplanationMultiError, or nil if none found.
+func (m *ActionExplanation) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ActionExplanation) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if all {
+		switch v := interface{}(m.GetTs()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ActionExplanationValidationError{
+					field:  "Ts",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ActionExplanationValidationError{
+					field:  "Ts",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetTs()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ActionExplanationValidationError{
+				field:  "Ts",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	// no validation rules for ActionType
+
+	// no validation rules for Message
+
+	if len(errors) > 0 {
+		return ActionExplanationMultiError(errors)
+	}
+
+	return nil
+}
+
+// ActionExplanationMultiError is an error wrapping multiple validation errors
+// returned by ActionExplanation.ValidateAll() if the designated constraints
+// aren't met.
+type ActionExplanationMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ActionExplanationMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ActionExplanationMultiError) AllErrors() []error { return m }
+
+// ActionExplanationValidationError is the validation error returned by
+// ActionExplanation.Validate if the designated constraints aren't met.
+type ActionExplanationValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ActionExplanationValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ActionExplanationValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ActionExplanationValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ActionExplanationValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ActionExplanationValidationError) ErrorName() string {
+	return "ActionExplanationValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ActionExplanationValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sActionExplanation.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ActionExplanationValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ActionExplanationValidationError{}
+
 // Validate checks the field values on Version with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
@@ -2253,6 +2388,35 @@ func (m *ControlState) validate(all bool) error {
 			}
 		}
 
+	}
+
+	if all {
+		switch v := interface{}(m.GetActionExplanation()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ControlStateValidationError{
+					field:  "ActionExplanation",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ControlStateValidationError{
+					field:  "ActionExplanation",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetActionExplanation()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ControlStateValidationError{
+				field:  "ActionExplanation",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
 	}
 
 	if len(errors) > 0 {
