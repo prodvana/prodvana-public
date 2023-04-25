e96df5690f9eda262efd42cb8d88289d09b43f12
Naphat Sanguansin <naphat@prodvana.io>
2023-04-24 21:35:28 -0700

    almost wire up protection execution
    This is currently breaking because of runtime object's dependency on rc config, as part of getEntity. I will fix that separately as this is getting pretty big.
    
     ayf
    
    GitOrigin-RevId: 6e80ea5354b7ddbf3ce941c0482a3a4af9feeb43

--- go/prodvana-sdk/proto/prodvana/desired_state/model/desired_state.pb.validate.go
+++ go/prodvana-sdk/proto/prodvana/desired_state/model/desired_state.pb.validate.go
@@ -3861,7 +3861,39 @@ func (m *ProtectionAttachment) validate(all bool) error {
 		}
 	}
 
-	// no validation rules for Version
+	for idx, item := range m.GetVersions() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ProtectionAttachmentValidationError{
+						field:  fmt.Sprintf("Versions[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ProtectionAttachmentValidationError{
+						field:  fmt.Sprintf("Versions[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ProtectionAttachmentValidationError{
+					field:  fmt.Sprintf("Versions[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
 
 	// no validation rules for ProtectionId
 
