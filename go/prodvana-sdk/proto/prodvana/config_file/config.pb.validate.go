1e6b3b47f387d9082f4ce1a828203ea2a692060e
Naphat Sanguansin <naphat@prodvana.io>
2023-05-17 22:27:58 -0400

    delivery module as top level model
    this doesn't yet integrate it into DD
    
     ayf
    
    GitOrigin-RevId: 783f584f087255260e6aaf2b8b1065d4fab94020

--- go/prodvana-sdk/proto/prodvana/config_file/config.pb.validate.go
+++ go/prodvana-sdk/proto/prodvana/config_file/config.pb.validate.go
@@ -227,6 +227,48 @@ func (m *ProdvanaConfig) validate(all bool) error {
 			}
 		}
 
+	case *ProdvanaConfig_DeliveryModule:
+		if v == nil {
+			err := ProdvanaConfigValidationError{
+				field:  "ConfigOneof",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+		oneofConfigOneofPresent = true
+
+		if all {
+			switch v := interface{}(m.GetDeliveryModule()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ProdvanaConfigValidationError{
+						field:  "DeliveryModule",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ProdvanaConfigValidationError{
+						field:  "DeliveryModule",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetDeliveryModule()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ProdvanaConfigValidationError{
+					field:  "DeliveryModule",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
